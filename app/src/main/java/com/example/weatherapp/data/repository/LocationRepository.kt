6af1a7bc8e32e9b9b9ff49ddc2e066617718a7ce
Chayapol Chaipongsawalee <chayapol.c@arise.tech>
2024-07-27 21:32:55 +0700

    add location repo method

--- app/src/main/java/com/example/weatherapp/data/repository/LocationRepository.kt
+++ app/src/main/java/com/example/weatherapp/data/repository/LocationRepository.kt
@@ -1,10 +1,16 @@
 package com.example.weatherapp.data.repository
 
+import android.location.Location
 import com.google.android.gms.location.LocationResult
+import kotlinx.coroutines.flow.Flow
 
 interface LocationRepository {
 
     fun updateLocation(callback: (LocationResult) -> Unit)
 
     fun stopLocationUpdate()
+
+    fun hasLocationPermission(): Boolean
+
+    fun listenToLocation(): Flow<Location>
 }
--- app/src/main/java/com/example/weatherapp/data/repository/LocationRepositoryImpl.kt
+++ app/src/main/java/com/example/weatherapp/data/repository/LocationRepositoryImpl.kt
@@ -1,21 +1,34 @@
 package com.example.weatherapp.data.repository
 
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Application
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Looper
+import androidx.core.content.ContextCompat
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class LocationRepositoryImpl @Inject constructor(
-    activity: Application
+    private val activity: Application
 ) : LocationRepository {
-    private val fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity)
+    private val fusedLocationClient by lazy {
+        LocationServices.getFusedLocationProviderClient(activity)
+    }
     private val locationCallback = object : LocationCallback() {
         override fun onLocationResult(locationResult: LocationResult) {
+            super.onLocationResult(locationResult)
             onLocationUpdateCallback?.invoke(locationResult)
         }
     }
@@ -24,9 +37,12 @@ class LocationRepositoryImpl @Inject constructor(
 
     @SuppressLint("MissingPermission")
     override fun updateLocation(callback: (LocationResult) -> Unit) {
-        val locationRequest = LocationRequest().apply {
-            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        }
+        val locationRequest = LocationRequest.Builder(
+            Priority.PRIORITY_BALANCED_POWER_ACCURACY,
+            TimeUnit.MINUTES.toMillis(10)
+        ).apply {
+            setMinUpdateIntervalMillis(1000L)
+        }.build()
         onLocationUpdateCallback = callback
 
         fusedLocationClient.requestLocationUpdates(
@@ -40,4 +56,48 @@ class LocationRepositoryImpl @Inject constructor(
         onLocationUpdateCallback = null
         fusedLocationClient.removeLocationUpdates(locationCallback)
     }
+
+    override fun hasLocationPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            activity.applicationContext,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun listenToLocation(): Flow<Location> {
+        val request = LocationRequest.Builder(
+            Priority.PRIORITY_BALANCED_POWER_ACCURACY,
+            TimeUnit.MINUTES.toMillis(10)
+        ).apply {
+            setMinUpdateIntervalMillis(1000L)
+        }.build()
+
+        return callbackFlow {
+            if (hasLocationPermission().not()) {
+                throw NoPermissionsException
+            }
+            val locationCallback = object : LocationCallback() {
+                override fun onLocationResult(result: LocationResult) {
+                    super.onLocationResult(result)
+                    result.lastLocation?.let {
+                        launch {
+                            send(it)
+                        }
+                    }
+                }
+            }
+            fusedLocationClient.requestLocationUpdates(
+                request,
+                locationCallback,
+                Looper.getMainLooper(),
+            )
+
+            awaitClose {
+                fusedLocationClient.removeLocationUpdates(locationCallback)
+            }
+        }
+    }
+
+    object NoPermissionsException : Exception()
 }
